54e8ffb3191fd2dab64c063f21738960273be9b8
William Meier <23337498+WilliamMeier@users.noreply.github.com>
2022-01-07 15:45:48 -0300

    multiplos eventos

--- 2_project_vue/src/components/PrimeiroComponente.vue
+++ 2_project_vue/src/components/PrimeiroComponente.vue
@@ -2,12 +2,18 @@
   <div>
     <h1>Olá Vue!</h1>
     <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
+    <MultiplosEventos />
   </div>
 </template>
 
 <script>
+import MultiplosEventos from './MultiplosEventos.vue'
+
 export default {
   name: 'PrimeiroComponente',
+  components: {
+    MultiplosEventos,
+  },
   data() {
     return {
       nome: 'William',
