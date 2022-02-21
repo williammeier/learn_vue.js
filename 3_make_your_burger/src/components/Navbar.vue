c530061ccb3f516eefd367edd18995089cc1f6ee
William Meier <william.meier@outlook.com>
2022-02-21 10:58:55 -0300

    course project changes

---
+++ 3_make_your_burger/src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="nav">
+    <router-link to="/" id="logo-url">
+      <img id="logo" :src="logo" :alt="alt" />
+    </router-link>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/pedidos">Pedidos</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  props: ['logo', 'alt'],
+}
+</script>
+
+<style>
+#nav {
+  background-color: #222;
+  border-bottom: 1px solid #111;
+  padding: 15px 50px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+
+#nav #logo-url {
+  margin: auto;
+  margin-left: 0;
+}
+#logo {
+  height: 40px;
+}
+#nav a {
+  color: #fcba03;
+  text-decoration: none;
+  margin: 2px;
+  transition: 0.5s;
+}
+#nav a:hover {
+  color: #fff;
+}
+</style>
---
+++ 3_make_your_burger/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <Banner />
+    <div class="main-container">
+      <h1>Monte o seu Burger</h1>
+      <p>formulario</p>
+    </div>
+  </section>
+</template>
+
+<script>
+import Banner from '../components/Banner.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Banner,
+  },
+}
+</script>
